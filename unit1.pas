df8a10f72a371711224c67e2b578f31728ff11ce
Vlad-Obraz <118714709+Vlad-Obraz@users.noreply.github.com>
2022-12-06 10:12:00 +0700

    Rename, moving back forward
    Добавлено в Uses: ExtCtrls, Buttons
    
    Добавлено в var: OldName: string
    
    Добавлены procedure: FormKeyDown, FormMouseDown, ShellListView1Edited, ShellListView1Editing, ShellListView1KeyDown, ShellListView1MouseDown, ArrowBackClick, ArrowForwardClick

--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ShellCtrls, ComCtrls,
-  StdCtrls, LazUTF8, FileOpenModule;
+  StdCtrls, ExtCtrls, Buttons, LazUTF8, FileOpenModule;
 
 type
 
@@ -15,14 +15,29 @@ type
   TForm1 = class(TForm)
     Button1: TButton;
     ComboBox1: TComboBox;
+    ArrowBack: TImage;
+    ArrowForward: TImage;
     Label1: TLabel;
     ShellListView1: TShellListView;
     ShellTreeView1: TShellTreeView;
     StatusBar1: TStatusBar;
     ToolBar1: TToolBar;
+    procedure ArrowBackClick(Sender: TObject);
+    procedure ArrowForwardClick(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure ComboBox1Select(Sender: TObject);
+    procedure FormKeyDown(Sender: TObject; var Key: word; Shift: TShiftState);
+    procedure FormMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: integer);
     procedure ShellListView1DblClick(Sender: TObject);
+    procedure ShellListView1Edited(Sender: TObject; Item: TListItem;
+      var AValue: string);
+    procedure ShellListView1Editing(Sender: TObject; Item: TListItem;
+      var AllowEdit: boolean);
+    procedure ShellListView1KeyDown(Sender: TObject; var Key: word;
+      Shift: TShiftState);
+    procedure ShellListView1MouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: integer);
     procedure ShellListView1SelectItem(Sender: TObject; Item: TListItem;
       Selected: boolean);
   private
@@ -33,7 +48,7 @@ type
 
 var
   Form1: TForm1;
-  Path: string;
+  Path, OldName: string;
 
 implementation
 
@@ -62,27 +77,164 @@ begin
 
 end;
 
+procedure TForm1.FormKeyDown(Sender: TObject; var Key: word; Shift: TShiftState);
+begin
+
+  {Alt + Стрелка влево}
+  if ((key = $25) and (ssAlt in Shift)) then
+    try
+      ShellListView1.Root := ExtractFileDir(ShellListView1.Root);
+    except
+    end;
+
+  {Alt + Стрелка вправо}
+  if ((key = $27) and (ssAlt in Shift)) then
+  begin
+    if (ExtractFileExt(Path) = '') then
+      try
+        ShellListView1.Root := ExtractFileDir(ExtractFileDir(Path));
+      except
+      end;
+  end;
+
+end;
+
+procedure TForm1.FormMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: integer);
+begin
+
+  {Mouse XButton1}
+  if (Button = mbExtra2) then
+  begin
+    if (ExtractFileExt(Path) = '') then
+      try
+        ShellListView1.Root := ExtractFileDir(ExtractFileDir(Path));
+      except
+      end;
+  end;
+
+  {Mouse XButton2}
+  if (Button = mbExtra1) then
+    try
+      ShellListView1.Root := ExtractFileDir(ShellListView1.Root);
+    except
+    end;
+
+end;
+
 procedure TForm1.ShellListView1DblClick(Sender: TObject);
 begin
 
+  {Открытие папки}
   if (ExtractFileExt(Path) = '') then
   begin
     ShellListView1.Root := Path;
   end;
 
+  {Открытие файлов}
   FileOpen(Path);
+
+end;
+
+procedure TForm1.ShellListView1Edited(Sender: TObject; Item: TListItem;
+  var AValue: string);
+begin
+
+  {Переименование}
+  RenameFile(ExtractFilePath(Path) + OldName, ExtractFilePath(Path) + AValue);
+
+end;
+
+procedure TForm1.ShellListView1Editing(Sender: TObject; Item: TListItem;
+  var AllowEdit: boolean);
+begin
+
+  {Имя для переименования}
+  OldName := ExtractFileName(Path);
+
+end;
+
+procedure TForm1.ShellListView1KeyDown(Sender: TObject; var Key: word;
+  Shift: TShiftState);
+begin
+
+  {Alt + Стрелка влево}
+  if ((key = $25) and (ssAlt in Shift)) then
+    try
+      ShellListView1.Root := ExtractFileDir(ShellListView1.Root);
+    except
+    end;
+
+  {Alt + Стрелка вправо}
+  if ((key = $27) and (ssAlt in Shift)) then
+  begin
+    if (ExtractFileExt(Path) = '') then
+      try
+        ShellListView1.Root := ExtractFileDir(ExtractFileDir(Path));
+      except
+      end;
+  end;
+
+end;
+
+procedure TForm1.ShellListView1MouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: integer);
+begin
+
+  {Mouse XButton1}
+  if (Button = mbExtra2) then
+  begin
+    if (ExtractFileExt(Path) = '') then
+      try
+        ShellListView1.Root := ExtractFileDir(ExtractFileDir(Path));
+      except
+      end;
+  end;
+
+  {Mouse XButton2}
+  if (Button = mbExtra1) then
+    try
+      ShellListView1.Root := ExtractFileDir(ShellListView1.Root);
+    except
+    end;
 
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
 
+  //showmessage(ExtractFileDir(ExtractFileDir(ExtractFileDir(Path))));
+  ShowMessage(ShellListView1.Root + '\' + ExtractFileName(Path));
+  //showmessage(extractfiledir(extractfiledir(path)));
   try
     ShellListView1.Root := StatusBar1.SimpleText;
   except
     on E: EInvalidPath do
       ShowMessage('Я ещё не умею открывать файлы :(');
   end;
+
+end;
+
+procedure TForm1.ArrowBackClick(Sender: TObject);
+begin
+
+  {Стрелка назад}
+  try
+    ShellListView1.Root := ExtractFileDir(ShellListView1.Root);
+  except
+  end;
+
+end;
+
+procedure TForm1.ArrowForwardClick(Sender: TObject);
+begin
+
+  {Стрелка вперед}
+  if (ExtractFileExt(Path) = '') then
+    try
+      ShellListView1.Root := ExtractFileDir(ExtractFileDir(Path));
+    except
+    end;
 
 end;
 
